4cec40ffd8e59c0b004f822b2adc763826b30823
José Vicente del Valle Fayos <musheres.dioxinas.0p@icloud.com>
2024-03-02 00:54:47 +0100

    quitada la coletilla api de las rutas

--- rutas/rutas.go
+++ rutas/rutas.go
@@ -7,28 +7,28 @@ import (
 )
 
 func Configuracion(app *fiber.App) {
-	// app.Post("/api/registrar", controladores.Registrar)
-	app.Post("/api/ingresar", controladores.Ingresar)
-	app.Get("/api/entrada/:id", controladores.LeerEntrada)
-	app.Get("/api/entradas", controladores.TodasEntradas)   // Muestra las entradas paginadas
-	app.Get("/api/todas", controladores.EntradasSinPaginar) // Muestra las entradas paginadas
-	app.Get("/api/especial", controladores.TodasEntradas)   // Muestra las entradas ver parametros del query
-	app.Get("/api/comentarios", controladores.TodosComentarios)
+	// app.Post("/registrar", controladores.Registrar)
+	app.Post("/ingresar", controladores.Ingresar)
+	app.Get("/entrada/:id", controladores.LeerEntrada)
+	app.Get("/entradas", controladores.TodasEntradas)   // Muestra las entradas paginadas
+	app.Get("/todas", controladores.EntradasSinPaginar) // Muestra las entradas paginadas
+	app.Get("/especial", controladores.TodasEntradas)   // Muestra las entradas ver parametros del query
+	app.Get("/comentarios", controladores.TodosComentarios)
 	// app.Post("/api/KzQ987h29KkYem", controladores.Escribeme)
 	app.Use(middlewares.Autenticado)
-	app.Get("/api/usuario", controladores.Usuario)
-	app.Post("/api/salir", controladores.Salir)
-	app.Post("/api/usuario", controladores.CrearUsuario)
-	app.Post("/api/entrada", controladores.CrearEntrada)
-	app.Post("/api/comentario", controladores.CrearComentario)
-	app.Get("/api/usuario/:id", controladores.LeerUsuario)
-	app.Get("/api/comentario/:id", controladores.LeerComentario)
-	app.Put("/api/usuario/:id", controladores.ActualizarUsuario)
-	app.Put("/api/entrada/:id", controladores.ActualizarEntrada)
-	app.Put("/api/comentario/:id", controladores.ActualizarComentario)
-	app.Delete("/api/usuario/:id", controladores.BorrarUsuario)
-	app.Delete("/api/entrada/:id", controladores.BorrarEntrada)
-	app.Delete("/api/comentario/:id", controladores.BorrarComentario)
-	app.Get("/api/usuarios", controladores.TodosUsuarios)
+	app.Get("/usuario", controladores.Usuario)
+	app.Post("/salir", controladores.Salir)
+	app.Post("/usuario", controladores.CrearUsuario)
+	app.Post("/entrada", controladores.CrearEntrada)
+	app.Post("/comentario", controladores.CrearComentario)
+	app.Get("/:id", controladores.LeerUsuario)
+	app.Get("/comentario/:id", controladores.LeerComentario)
+	app.Put("/usuario/:id", controladores.ActualizarUsuario)
+	app.Put("/entrada/:id", controladores.ActualizarEntrada)
+	app.Put("/comentario/:id", controladores.ActualizarComentario)
+	// app.Delete("/usuario/:id", controladores.BorrarUsuario)
+	app.Delete("/entrada/:id", controladores.BorrarEntrada)
+	app.Delete("/comentario/:id", controladores.BorrarComentario)
+	app.Get("/usuarios", controladores.TodosUsuarios)
 
 }
