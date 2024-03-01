26e79f6f07648810b8f0359c3fd16f55be3e55df
José Vicente del Valle Fayos <musheres.dioxinas.0p@icloud.com>
2024-03-01 22:03:10 +0100

    modificada configuracion cors

--- main.go
+++ main.go
@@ -12,10 +12,11 @@ func main() {
 	bbdd.Conectar()
 	app := fiber.New()
 	app.Use(cors.New(cors.Config{
-		AllowHeaders:     "Origin, Content-Type, Accept, Content-Length, Accept-Language, Accept-Encoding, Connection, Access-Control-Allow-Origin",
+		AllowHeaders:     "Origin, Content-Type, Accept, Content-Length, Accept-Language, Accept-Encoding, Connection, Access-Control-Allow-Origin, Authorization",
 		AllowOrigins:     "https://www.nuestrodiario.es, http://localhost:3000",
 		AllowCredentials: true,
 		AllowMethods:     "GET, POST, HEAD, PUT, DELETE, PATCH, OPTIONS",
+		MaxAge:           86400,
 	}))
 	rutas.Configuracion(app)
 	app.Listen(":" + os.Getenv("PORT"))
